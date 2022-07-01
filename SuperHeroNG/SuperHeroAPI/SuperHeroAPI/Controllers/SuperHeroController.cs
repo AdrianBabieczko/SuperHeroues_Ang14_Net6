fb7e638703d3c27ac33bb9830ce2789b8df3c340
Adrian Babieczko <adrian.babieczko@gmail.com>
2022-07-01 11:47:26 +0200

    Add CRUD methods to API

--- SuperHeroNG/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroNG/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -20,5 +20,48 @@ namespace SuperHeroAPI.Controllers
         {
             return Ok(await _context.SuperHeroes.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero is null)
+            {
+                return BadRequest("Hero not found.");
+            }
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.Place = hero.Place;
+            dbHero.LastName = hero.LastName;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+            if (dbHero is null)
+            {
+                return BadRequest("Hero not found.");
+            }
+
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+            
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
     }
 }
